05011e7dfba71185845c47846eed67a19ace5f41
Venice-123 <149569218+Venice-123@users.noreply.github.com>
2023-11-07 13:48:46 +0300

    Add files via upload

---
+++ hmanna.js
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Vegetarian Food Restaurant</title>
+        <link rel="stylesheet" type="text/css" href="style.css"
+    </head>
+    <body>
+        <header>
+            <h1>Vegetarian Food Restaurant<h1>
+            <nav>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="services">Services</a></li>
+                    <li><a href="#contact">Contact</a><li>
+                    </u1>
+                </nav>
+            </header>
+
+            <section id="home">
+                <h2>Welcome to our Vegetarian Food Restaurant!</h2>
+                <p>Enjoy delicious and healthy vegetarian meals made with fresh ingredients.Your Health Our Priority.</p>
+            </section>
+
+            <section id="menu">
+                <h2>Our Menu</h2>
+                <u1>
+                    <li>Vegetable and Fruit Salads</li>
+                    <li>Fruits</li>
+                    <li>Uji Power</li>
+                    <li>Chapo</li>
+                    <li>Githeri</li>
+                    <li>Ugali+Kienyeji</li>
+                    <li>Juices for blood pressure,Ulcers,Cancer,Diabetes and Drug Addictions</li>
+                </u1>
+            </section>
+
+            <section id="about">
+                <h2>About Us</h2>
+                <p>We are a Vegetarian food restaurant committed to providing delicious and nutritious meals to our customers.Our menu is carefully crafted to cater for various dietary prefernces and restrictions.</P>
+            </section>
+            
+            <section id="services">
+                <h2>We offer services suitable for our customers</h2>
+                <u1>
+                    <li>Outside Catering Services</li>
+                    <li>Culinary Classes</li>
+                    <li>Health Coaching</li>
+                    <li>Natural Remedies</li>
+                </u1>
+            </section>
+
+            <section id="contact">
+                <h2>Contact Us</h2>
+                <p>For Reservations or inquiries,please call us at: 07159990590/0742343911</p>
+            </section>
+
+            <footer>
+                <p>&copy; 2022 Vegetarian Food Restaurant.All rights reserved.</p>
+            </footer>
+        </body>
+        </html>
+
+        CSS code (style.css):
+
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #0F3325;
+            padding: 20px;
+        }
+        h1 {
+            margin: 0;
+        }
+        
+        nav u1 {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        nav u1 li {
+            display: inline;
+            margin-right: 10px;
+        }
+
+        nav u1 li q {
+            text-decoration: none;
+            color: #FFFFFF;
+        }
+
+        section {
+            padding: 50px;
+        }
+
+        footer {
+            backround-color: #0F3325
+            padding: 20px;
+            text-align: center;
+        }
+
+
